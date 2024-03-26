7b64c1fe10ee0d896b0b14f6508ec9ac3df6a84a
Edimar Cardoso <edimarlnx@gmail.com>
2024-03-26 15:29:51 -0300

    Update script version to latest: v0.0.1-alpha.8

--- scripts/update-install-version.sh
+++ scripts/update-install-version.sh
@@ -2,3 +2,5 @@
 
 VERSION=${1:-"$(git tag --list --sort=-committerdate | head -n 1)"}
 sed -i "s/INSTALL_VERSION=\${INSTALL_VERSION:-\".*\"}/INSTALL_VERSION=\${INSTALL_VERSION:-\"${VERSION}\"}/" scripts/install.sh
+git add scripts
+git commit -m "Update script version to latest: ${VERSION}"
