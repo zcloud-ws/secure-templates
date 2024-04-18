a75c9b2b17fc3e0e2fb5f40048fcaa57cd635cdb
Edimar Cardoso <edimarlnx@gmail.com>
2024-04-18 15:13:43 -0300

    Adding coverage test report

---
+++ coverage-update.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+set -e
+
+COVERAGE_CMD="
+go test -covermode=count -coverpkg=./... -coverprofile coverage/cover.out ./...
+go tool cover -html coverage/cover.out -o coverage/cover.html
+go tool cover -func coverage/cover.out | grep total | awk '{print \$3}' > coverage/total.txt
+"
+
+if [ "$RUN_IN_DOCKER" = "true" ]; then
+docker run --rm -it --name stpl-coverage -u $UID \
+  -v "$PWD":/source \
+  -e GOCACHE=/tmp/.cache \
+  -w /source \
+  golang:"$STPL_GO_VERSION" \
+  bash -c "$COVERAGE_CMD"
+else
+  bash -l -c "$COVERAGE_CMD"
+fi
+
+#go test -covermode=count -coverpkg=./... -coverprofile coverage/cover.out ./...
+#go tool cover -html coverage/cover.out -o coverage/cover.html
+#go tool cover -func coverage/cover.out | grep total | awk '{print $3}' > coverage/total.txt
+
+COVERAGE_TOTAL="$(cut -d. -f1 coverage/total.txt)"
+COLOR=lightgreen
+
+if ((COVERAGE_TOTAL < 70)); then
+  COLOR=orange
+fi
+if ((COVERAGE_TOTAL < 60)) ; then
+  COLOR=yellow
+fi
+if ((COVERAGE_TOTAL < 50)) ; then
+  COLOR=red
+fi
+
+echo "Update coverage to $COVERAGE_TOTAL% / $COLOR"
+
+sed -i "s/Coverage-[[:digit:].]*%25-[a-zA-Z]*/Coverage-$COVERAGE_TOTAL%25-$COLOR/g" README.md
+git add coverage
+git add README.md
+
+if [ "$COMMIT_COVERAGE_UPDATES" = "true" ]; then
+  git commit -m "Update coverage to ${COVERAGE_TOTAL}%" README.md coverage/
+fi
--- govulncheck-docker.sh
+++ govulncheck-docker.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
+set -e
+
+# Load Go version into STPL_GO_VERSION
+. .go-version
 
 docker run --rm -it --name govulncheck -u $UID \
-  -v $PWD:/source \
+  -v "$PWD":/source \
   -e GOCACHE=/tmp/.cache \
   -w /source \
-  golang:1.21.9 \
+  golang:"$STPL_GO_VERSION" \
   bash -c 'go install golang.org/x/vuln/cmd/govulncheck@latest && govulncheck -show verbose ./...' > govulncheck.log
